c9778eea37ddfd2e35070d10bdb6a57f96f8a9b7
mark <mark@protopie.io>
2017-01-27 15:29:22 +0900

    interface.ts 파일 녹음전 최종 멘트 수정

--- keyof_and_Lookup_Types/interface.ts
+++ keyof_and_Lookup_Types/interface.ts
@@ -8,14 +8,13 @@ function testFunc(person: {name: string, age: number, location: string}): void {
     console.log(person.name);
 }
 
-// testFunc 함수는 다음과 같은 형태로 사용할 수 있습니다.
+// 이렇게 작성된 함수는 다음과 같은 형태로 사용할 수 있습니다.
 testFunc({name: 'Mark', age: 35, location: 'seoul'});
-// 형태를 다르게 입력하면 제대로 트랜스파일이 되지 않습니다.
-// 이런식으로 런타임 전에 타입 체크를 통해 런타임 시 오류가 날 가능성을 줄입니다.
+// 지정된 모양과 다르게 입력하면 제대로 트랜스파일이 되지 않습니다.
 // testFunc({name: 'Mark', age: '35', location: 'seoul'});
+// 이런식으로 런타임 전에 타입 체크를 하면 런타임 시 오류가 날 가능성을 줄일 수 있습니다.
 
-// 위에서 쓰인 person 오브젝트의 모양을
-// 따로 인터페이스 키워드를 통하여, 미리 지정하고 사용할 수 있습니다.
+// 인터페이스 키워드를 사용하면, person 오브젝트의 모양을  미리 지정하고 사용할 수 있습니다.
 interface Person {
     name: string;
     age: number;
